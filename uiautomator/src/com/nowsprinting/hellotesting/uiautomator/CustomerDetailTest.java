0d88783be57697b8a5449c8b99c1f99b5940e31e
TOYAMA Sumio <jun.nama@gmail.com>
2014-09-22 22:46:29 +0900

    Set longer timeout value of clickAndWaitForWindow() for running on slow emulators.

--- uiautomator/src/com/nowsprinting/hellotesting/uiautomator/CustomerDetailTest.java
+++ uiautomator/src/com/nowsprinting/hellotesting/uiautomator/CustomerDetailTest.java
@@ -16,15 +16,24 @@
 
 package com.nowsprinting.hellotesting.uiautomator;
 
-import com.android.uiautomator.core.UiObject;
-import com.android.uiautomator.core.UiSelector;
-import com.android.uiautomator.testrunner.UiAutomatorTestCase;
-
+import android.os.Build;
+import android.os.SystemClock;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 
+import com.android.uiautomator.core.Configurator;
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
 public class CustomerDetailTest extends UiAutomatorTestCase {
+
+    /** clickAndWaitForWindow()のタイムアウト値 */
+    private static final long WAIT_TIMEOUT = 30 * 1000L;
+
+    /** 戻るボタン押下後にスリープする時間 */
+    private static final long PRESS_BACK_WAIT = 10 * 1000L;
 
     /** Master画面の[add]コンポーネント */
     private UiObject mAddButtonInMasterScreen;
@@ -41,11 +50,24 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
     /** Detail画面の[preview]コンポーネント */
     private UiObject mPreviewButtonInDetailScreen;
 
+    /**
+     * APIレベル18以降において、 clickAndWaitForWindow()のタイムアウト値を {@link #WAIT_TIMEOUT} に設定する。
+     * 
+     * @see https://code.google.com/p/android/issues/detail?id=75610
+     */
+    private void changeWaitTimeout() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            Configurator config = Configurator.getInstance();
+            config.setActionAcknowledgmentTimeout(WAIT_TIMEOUT);
+        }
+
+    }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
+        changeWaitTimeout();
         // 各UIコンポーネントを検索するためのUiObjectを構築する。
         // この時点UiObjectをnewした時点)では実際の検索は行われないことに注意。
         mAddButtonInMasterScreen = new UiObject(new UiSelector().description("add"));
@@ -57,12 +79,14 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
 
     /**
      * Detail画面の「年齢」入力欄を検索するためのUiObjectを生成する。
+     * 
      * <pre>
      * NumberPicker
      *   ├Button
      *   ├EditText (年齢の入力欄)
      *   └Button
      * </pre>
+     * 
      * というレイアウトになっているため、「NumberPicker配下のEditText」を検索条件とする。
      */
     private UiObject createAgeTextFieldInDetailScreen() {
@@ -73,11 +97,13 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
 
     /**
      * Detail画面の「男性」ラジオボタンを検索するためのUiObjectを生成する。
+     * 
      * <pre>
      * RadioGroup (contentDescription="gender segmentedcontrol")
      *  ├RadioButton ("男性")
      *  └RadioButton ("女性")
      * </pre>
+     * 
      * というレイアウトになっているため、
      * 「contentDescrption属性が"gender segmentedcontrol"」のコンポーネント配下の"男性"ラジオボタン」
      * を検索条件とする。
@@ -91,14 +117,14 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
     /**
      * 新規顧客を追加できることを確認する。
      * <ul>
-     *     <li>Master画面で[add]ボタンを押してDetail画面に遷移する。</li>
-     *     <li>名前入力欄に"Newton Geizler"と入力する。</li>
-     *     <li>性別欄で「男性」を選択する。</li>
-     *     <li>年齢欄に「35」と入力する。</li>
-     *     <li>[preview]ボタンを押して、プレビュー画面に遷移する。</li>
-     *     <li>「戻る」ボタンを押してDetail画面に戻る。</li>
-     *     <li>Master画面に、さきほど入力した名前・性別・年齢が反映されていることを確認する。</li>
-     *     <li>「戻る」ボタンを押してMaster画面に戻る。</li>
+     * <li>Master画面で[add]ボタンを押してDetail画面に遷移する。</li>
+     * <li>名前入力欄に"Newton Geizler"と入力する。</li>
+     * <li>性別欄で「男性」を選択する。</li>
+     * <li>年齢欄に「35」と入力する。</li>
+     * <li>[preview]ボタンを押して、プレビュー画面に遷移する。</li>
+     * <li>「戻る」ボタンを押してDetail画面に戻る。</li>
+     * <li>Master画面に、さきほど入力した名前・性別・年齢が反映されていることを確認する。</li>
+     * <li>「戻る」ボタンを押してMaster画面に戻る。</li>
      * </ul>
      */
     public void test新規顧客の追加() throws Exception {
@@ -109,7 +135,7 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
         try {
             // exercise
             // addボタンを押し、Detail画面が切り替わるまで待つ。
-            mAddButtonInMasterScreen.clickAndWaitForNewWindow();
+            mAddButtonInMasterScreen.clickAndWaitForNewWindow(WAIT_TIMEOUT);
             // 名前入力欄テキストを入力する。
             mNameTextFieldInDetailScreen.setText(inputName);
             // 性別欄の「男性」をクリックする。
@@ -118,11 +144,12 @@ public class CustomerDetailTest extends UiAutomatorTestCase {
             mAgeTextFieldInDetailScreen.setText(inputAge);
             mAgeTextFieldInDetailScreen.click();
             // previewボタンを押して、プレビュー画面に切り替わるまで待つ。
-            mPreviewButtonInDetailScreen.clickAndWaitForNewWindow();
+            mPreviewButtonInDetailScreen.clickAndWaitForNewWindow(WAIT_TIMEOUT);
 
-            // 戻るボタンを押し、アイドルになるまで待つ。これでDetail画面に戻る。
+            // 戻るボタンを押し、一定時間待つ。これでDetail画面に戻る。
             getUiDevice().pressBack();
-            getUiDevice().waitForIdle();
+            // 戻るボタンを押してから、画面が切り替わるまで待つ方法が用意されていないため、一定時間スリープする。
+            SystemClock.sleep(PRESS_BACK_WAIT);
 
             // verify
             // Detail画面で入力内容が保持されているか確認する。
